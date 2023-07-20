b09198561a04fbb9b82c874bd9c8001e9fbdda54
Arwaz Chisti <136723333+TheAbArwaz@users.noreply.github.com>
2023-07-20 18:08:28 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class loops {
+    public static void main(String[] args) {
+//        for (int counter = 0; counter < 3; counter = counter + 1) {
+//            System.out.println("assalam");
+//        }
+        //print numbers from 0 to 10
+//        for (int counter = 0; counter < 11; counter++) {
+//            System.out.println(counter + " ");
+//        int i = 0;
+//        while (i < 11) {
+//            System.out.println(i);
+//            i = i + 1;
+//        int i = 0;
+//        do {
+//            System.out.println(i);
+//            i = i + 1;
+//        }while (i<11);
+
+        // print sum of n natural num
+       Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+//        int sum = 0;
+//        for (int i = 0; i <= n; i++){
+//            sum = sum + i;
+//        }
+//        System.out.println(sum);
+        //print table of a number
+        for (int i = 1; i<11; i++){
+            System.out.println(i*n);
+
+        }
+        }
+    }
+
